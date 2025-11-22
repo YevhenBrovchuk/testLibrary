7776169304c9180ca04b7f2fdc8d297f4eb81ef0
YevhenBrovchuk <brovchukyevhen@ukr.net>
2025-11-22 20:51:25 +0200

    render arr img

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
+// Change code below this line
+const ulEl = document.querySelector(".gallery");
+console.log(ulEl);
 console.log(galleryItems);
+
+const marLi = galleryItems
+  .map(function ({ description, preview }) {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    // if (description.includes("H")) {
+    //   return `<li><img src="${preview}" alt="${description}"></li>`;
+    // } else {
+    //   return `<li>${description}</li>`;
+    // }
+  })
+  .join("");
+console.dir(marLi);
+ulEl.insertAdjacentHTML("beforeend", marLi);
